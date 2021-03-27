a4806ec73f124e756402528c5f0c01ff7e775f7d
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-03-27 13:27:14 +0200

    Add google map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import ExampleComponent from './ExampleComponent/ExampleComponent';
-import store from './root/store';
+import React from "react";
+import { Provider } from "react-redux";
+import SimpleMapPage from "./map/simple_map_page";
+import store from "./root/store";
 
-export default class App extends React.Component{
-  render(){
-    return (
-      <Provider store={store}>
-        <ExampleComponent />
-      </Provider>
-    );
-  }
+export default class App extends React.Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<SimpleMapPage />
+			</Provider>
+		);
+	}
 }
---
+++ client/src/map/my_great_place_styles.js
@@ -0,0 +1,27 @@
+const K_SIZE = 40;
+
+const greatPlaceStyle = {
+	// initially any map object has left top corner at lat lng coordinates
+	// it's on you to set object origin to 0,0 coordinates
+	position: "absolute",
+	width: 40,
+	height: 40,
+	left: 40 / 2,
+	top: -40 / 2,
+
+	// border: "5px solid #f44336",
+	// backgroundColor: "white",
+	borderRadius: K_SIZE,
+	textAlign: "center",
+	fontSize: 10,
+	fontWeight: "bold",
+	cursor: "pointer",
+};
+
+const greatPlaceStyleHover = {
+	...greatPlaceStyle,
+	border: "5px solid #3f51b5",
+	color: "#f44336",
+};
+
+export { greatPlaceStyle, greatPlaceStyleHover, K_SIZE };
