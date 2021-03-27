3fb8cf596e9381e2ea0b90defbab1fbcb7120986
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-03-27 18:50:03 +0200

    Added button to add event

--- client/src/map/filter.jsx
+++ client/src/map/filter.jsx
@@ -1,23 +1,25 @@
-import React, { PureComponent } from "react";
-import { Button, Form, Item, Segment, Select } from 'semantic-ui-react'
+import React, { PureComponent } from 'react';
+import { Button, Form, Item, Segment, Select } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from "redux";
+import { bindActionCreators } from 'redux';
 
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
 import { DateRangePicker } from 'react-date-range';
 import { getEventsFromDbWithFilter } from '../AddEventPage/eventActions';
+import { uk } from 'react-date-range/dist/locale';
+import { navigate } from '@reach/router';
 
-class Filter extends PureComponent{
-    constructor(props){
+class Filter extends PureComponent {
+    constructor(props) {
         super(props);
-        this.state={
-            startDate : new Date(),
-            endDate : new Date(),
+        this.state = {
+            startDate: new Date(),
+            endDate: new Date(),
             name: '',
             type: '',
-        }
+        };
     }
 
     onSubmit = () => {
@@ -26,9 +28,9 @@ class Filter extends PureComponent{
             type: this.state.type,
             createdAt: [this.state.startDate, this.state.endDate],
         });
-    }
+    };
 
-    changeField = (keyword) => (e, {value}) => {
+    changeField = (keyword) => (e, { value }) => {
         this.setState({
             [keyword]: value,
         });
@@ -36,77 +38,87 @@ class Filter extends PureComponent{
 
     changeFieldDate = () => (e) => {
         this.setState({
-            ["startDate"] : e.selection.startDate,
-            ["endDate"] : e.selection.endDate
-        })
-    }
-    render(){
+            ['startDate']: e.selection.startDate,
+            ['endDate']: e.selection.endDate,
+        });
+    };
+    render() {
         return (
             <Segment>
-            <Form>
-                <Form.Group widths="equal">
-                    <Form.Input
-                        value={this.state.name}
-                        onChange={this.changeField('name')}
-                        fluid
-                        label="Назва події:"
-                        placeholder="Щось сталось..."
-                    />
-                </Form.Group>
-                <Form.Group widths="equal">
-                    <Form.Field
-                        value={this.state.type}
-                        onChange={this.changeField('type')}
-                        control={Select}
-                        label="Тип події:"
-                        options={[
-                            { key: '', text: '', value: '' },
-                            { key: 'fire', text: 'Пожежа', value: 'fire' },
-                            { key: 'flod', text: 'Паводок', value: 'flod' },
+                <Form>
+                    <Form.Group widths="equal">
+                        <Form.Input
+                            value={this.state.name}
+                            onChange={this.changeField('name')}
+                            fluid
+                            label="Назва події:"
+                            placeholder="Щось сталось..."
+                        />
+                    </Form.Group>
+                    <Form.Group widths="equal">
+                        <Form.Field
+                            value={this.state.type}
+                            onChange={this.changeField('type')}
+                            control={Select}
+                            label="Тип події:"
+                            options={[
+                                { key: '', text: '', value: '' },
+                                { key: 'fire', text: 'Пожежа', value: 'fire' },
+                                { key: 'flod', text: 'Паводок', value: 'flod' },
+                                {
+                                    key: 'looting',
+                                    text: 'Пограбування / мародерство',
+                                    value: 'looting',
+                                },
+                                { key: 'murder', text: 'Вбивство', value: 'murder' },
+                                { key: 'accident', text: 'Аварія', value: 'accident' },
+                            ]}
+                            placeholder="Оберіть тип інциденту"
+                        />
+                    </Form.Group>
+                    <DateRangePicker
+                        ranges={[
                             {
-                                key: 'looting',
-                                text: 'Пограбування / мародерство',
-                                value: 'looting',
+                                startDate: this.state.startDate,
+                                endDate: this.state.endDate,
+                                key: 'selection',
                             },
-                            { key: 'murder', text: 'Вбивство', value: 'murder' },
-                            { key: 'accident', text: 'Аварія', value: 'accident' },
                         ]}
-                        placeholder="Оберіть тип інциденту"
+                        onChange={this.changeFieldDate()}
+                        locale={uk}
                     />
-                </Form.Group>
-                <DateRangePicker
-                    ranges={[{
-                        startDate: this.state.startDate,
-                        endDate: this.state.endDate,
-                        key: 'selection',
-                      }]}
-                    onChange={this.changeFieldDate()}
-                />
-                <Button onClick={this.onSubmit} type='submit'>Submit</Button>
-            </Form>
-            <Item>
-                <Item.Content>
-                    <Item.Header>{this.props.foundData}</Item.Header>
-                </Item.Content>
-            </Item>
+                    <Form.Group unstackable widths={2}>
+                        <Button onClick={this.onSubmit} type="submit">
+                            Фільтрувати
+                        </Button>
+                        <Button onClick={() => navigate('/add')} type="submit">
+                            Додати подію
+                        </Button>
+                    </Form.Group>
+                </Form>
+                <Item>
+                    <Item.Content>
+                        <Item.Header>{this.props.foundData}</Item.Header>
+                    </Item.Content>
+                </Item>
             </Segment>
-        )
+        );
     }
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators(
-		{
+    return bindActionCreators(
+        {
             getEventsFromDbWithFilter,
-		},
-		dispatch,
-	);
+        },
+        dispatch
+    );
 }
 
 function mapStateToProps(state) {
-	return {
-        events: state.events.eventsData
-	};
+    return {
+        events: state.events.eventsData,
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Filter);
